0a1205df157ee7cb512a3122814944bbc184e242
AK <akkhan07420@gmail.com>
2022-12-13 13:17:48 +0500

    added useful function

--- usefulFunc.c
+++ usefulFunc.c
@@ -1,6 +1,8 @@
 #include "virusFunc.h"
 #include <stdio.h>
-#include <sys/stat.h>
+#include <sys/stat.h> // stat
+#include <dirent.h>   // show dir
+#include <unistd.h>   // rmdir
 
 int deleteFile(const filename[])
 {
@@ -24,5 +26,38 @@ int isDirectory(char *path)
     else
     {
         return S_ISDIR(statbuf.st_mode);
+    }
+}
+
+char listOfFilesAndFolder(char dirname[], char arrayOfFileAndFolder[])
+{
+    struct dirent *d;
+    DIR *dir = opendir(dirname);
+    if (dir != NULL)
+    {
+        int index = 0;
+        for (d = readdir(dir); d != NULL; d = readdir(dir))
+        {
+            arrayOfFileAndFolder[index] = d->d_name;
+            index++;
+        }
+        closedir(dir);
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int removeEmptyDir(char dirname[])
+{
+    if (rmdir(dirname) == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
