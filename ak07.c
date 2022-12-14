6933e5b8145144173aece4fffbccf49824cf947c
AK <akkhan07420@gmail.com>
2022-12-14 00:22:45 -0800

    added to compile my virus

--- ak07.c
+++ ak07.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 #include "virusFunc.h"
 
-int main(void)
-{
+
+int main(int argc, char * argv[]){
+
+    char existDrivesName[20][10] = {};
+
+    detectDrives(existDrivesName);
+
+    printf("## in my man file lllllllllll\n");
+    for (int i = 0; i<3; i++){
+        printf("%s\n", existDrivesName[i]);
+    }
 
     return 0;
 }
---
+++ compileInWindow.sh
@@ -0,0 +1,1 @@
+gcc -o ak07 ak07.c usefulFunc.c && ./ak07
